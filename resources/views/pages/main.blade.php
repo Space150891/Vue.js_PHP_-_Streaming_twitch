e5b0b9c9a58716f13af9551242d2fe1cfc6b9618
Alex K <agent150891@gmail.com>
2018-10-11 12:02:46 +0300

    create blades

---
+++ resources/views/pages/main.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('template_title')
+	Welcome Gamificator
+@endsection
+
+@section('head')
+@endsection
+
+@section('content')
+
+ <div class="container">
+	<div class="row">
+	    <div class="col-md-12">
+			 main page
+        </div>
+    </div>
+</div>
+
+@endsection
